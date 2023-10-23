2d995472c371fe57ef35f1379d8d62c7989c4694
iqbalfatoni10 <mohiqbalfatoni10@gmail.com>
2023-10-23 13:44:32 +0700

    Add Images

--- lib/Pages/LoginPage.dart
+++ lib/Pages/LoginPage.dart
@@ -6,7 +6,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: SingleChildScrollView(
         child: Column(
           children: [
             SizedBox(
