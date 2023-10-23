3ff675e096b4c6f89293d59e65f2b757da123183
DavidTimoteus <2131710012@student.polinema.ac.id>
2023-10-23 20:08:41 +0700

    pembersihan

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'pages/WelcomeScreen.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'pages/LoginPage.dart';
-
 
 void main() {
   runApp(MyApp());
